6e733fc66e721c274082b4bb185b0742fc5bc094
Johannes Hernehult <jhernehult@gmail.com>
2026-03-04 12:05:30 +0100

    fixing fullwidth content

--- page.php
+++ page.php
@@ -9,13 +9,11 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-	<div class="container">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-		<?php endwhile; ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'template-parts/content', 'page' ); ?>
+	<?php endwhile; ?>
 
-	</div>
 </main>
 
 <?php
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,15 +8,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header>
-
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="entry-thumbnail">
-			<?php the_post_thumbnail( 'large' ); ?>
-		</div>
-	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php
