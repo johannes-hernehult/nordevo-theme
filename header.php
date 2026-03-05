e0e0a81ee830fc41649f4d474cc8f8a4dc5b7e42
Johannes Hernehult <jhernehult@gmail.com>
2026-03-05 11:04:48 +0100

    Removing navigation.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo("charset"); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
@@ -23,46 +23,12 @@
 		<div class="site-header__inner container">
 
 			<div class="site-branding">
-				<?php if ( has_custom_logo() ) : ?>
+				<?php if (has_custom_logo()): ?>
 					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-								<?php bloginfo( 'name' ); ?>
-							</a>
-						</h1>
-					<?php else : ?>
-						<p class="site-title">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-								<?php bloginfo( 'name' ); ?>
-							</a>
-						</p>
-					<?php endif; ?>
-					<?php
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo esc_html( $description ); ?></p>
-					<?php endif; ?>
+				<?php else: ?>
+					<?php bloginfo("name"); ?>
 				<?php endif; ?>
 			</div>
-
-			<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'nordevo-theme' ); ?>">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-					<span class="menu-toggle__bar"></span>
-					<span class="menu-toggle__bar"></span>
-					<span class="menu-toggle__bar"></span>
-					<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'nordevo-theme' ); ?></span>
-				</button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'primary',
-					'menu_id'        => 'primary-menu',
-					'container'      => false,
-				) );
-				?>
-			</nav>
-
 		</div>
 	</header>
 
--- inc/setup.php
+++ inc/setup.php
@@ -5,39 +5,31 @@
  * @package nordevo-theme
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined("ABSPATH")) {
+    exit();
 }
 
-function nordevo_setup() {
-	load_theme_textdomain( 'nordevo-theme', NORDEVO_DIR . '/languages' );
+function nordevo_setup()
+{
+    load_theme_textdomain("nordevo-theme", NORDEVO_DIR . "/languages");
 
-	add_theme_support( 'title-tag' );
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'html5', array(
-		'gallery',
-		'caption',
-		'style',
-		'script',
-	) );
-	add_theme_support( 'wp-block-styles' );
-	add_theme_support( 'align-wide' );
-	add_theme_support( 'responsive-embeds' );
-	add_theme_support( 'custom-logo', array(
-		'height'      => 60,
-		'width'       => 200,
-		'flex-height' => true,
-		'flex-width'  => true,
-	) );
-
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'nordevo-theme' ),
-		'footer'  => esc_html__( 'Footer Menu', 'nordevo-theme' ),
-	) );
+    add_theme_support("title-tag");
+    add_theme_support("post-thumbnails");
+    add_theme_support("html5", ["gallery", "caption", "style", "script"]);
+    add_theme_support("wp-block-styles");
+    add_theme_support("align-wide");
+    add_theme_support("responsive-embeds");
+    add_theme_support("custom-logo", [
+        "height" => 60,
+        "width" => 200,
+        "flex-height" => true,
+        "flex-width" => true,
+    ]);
 }
-add_action( 'after_setup_theme', 'nordevo_setup' );
+add_action("after_setup_theme", "nordevo_setup");
 
-function nordevo_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'nordevo_content_width', 1200 );
+function nordevo_content_width()
+{
+    $GLOBALS["content_width"] = apply_filters("nordevo_content_width", 1200);
 }
-add_action( 'after_setup_theme', 'nordevo_content_width', 0 );
+add_action("after_setup_theme", "nordevo_content_width", 0);
