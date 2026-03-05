0b9f2dd9a488f1d303f92a50076686a2e6ed1018
Johannes Hernehult <jhernehult@gmail.com>
2026-03-05 12:25:17 +0100

    Enqueueing content.css to editor. Styling Hero section

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,24 +5,36 @@
  * @package nordevo-theme
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined("ABSPATH")) {
+    exit();
 }
 
-function nordevo_scripts() {
-	wp_enqueue_style(
-		'nordevo-main',
-		NORDEVO_URI . '/assets/css/main.css',
-		array(),
-		NORDEVO_VERSION
-	);
+function nordevo_scripts()
+{
+    wp_enqueue_style(
+        "nordevo-main",
+        NORDEVO_URI . "/assets/css/main.css",
+        [],
+        NORDEVO_VERSION,
+    );
 
-	wp_enqueue_script(
-		'nordevo-main',
-		NORDEVO_URI . '/assets/js/main.js',
-		array(),
-		NORDEVO_VERSION,
-		true
-	);
+    wp_enqueue_style(
+        "nordevo-content",
+        NORDEVO_URI . "/assets/css/content.css",
+        [],
+        NORDEVO_VERSION,
+    );
+
+    wp_enqueue_script(
+        "nordevo-main",
+        NORDEVO_URI . "/assets/js/main.js",
+        [],
+        NORDEVO_VERSION,
+        true,
+    );
 }
-add_action( 'wp_enqueue_scripts', 'nordevo_scripts' );
+add_action("wp_enqueue_scripts", "nordevo_scripts");
+
+add_action("after_setup_theme", function () {
+    add_editor_style("assets/css/content.css");
+});
